e69eb308eade07efa75516d9503154e91ffbe06e
Masoud Moghini <koosha_pooyeshgar@outlook.com>
2017-08-19 19:46:45 +0430

    commitment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-
+import { AngularFireDatabase,
+  FirebaseListObservable
+,FirebaseObjectObservable } from 'angularfire2/database';
+import { Component,OnInit,OnDestroy } from '@angular/core';
+import 'rxjs/add/operator/map';
+import {Observable} from 'rxjs/Observable';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  cuisins:FirebaseListObservable<any[]>;
+  restaurants:Observable<any[]>;
+  constructor(private af:AngularFireDatabase){
+    
+  }
+
+  ngOnInit()
+  {
+    this.cuisins=this.af.list('/cuisines');
+    this.restaurants=this.af.list('/restaurants').map((restaurants)=>{
+      console.log("BEFORE MAP: ",restaurants);
+      restaurants.map((restaurant)=>{
+        restaurant.cuisineType=this.af.list('/cuisines'+restaurant.cuisine);
+      })
+        console.log('AFTER MAP',restaurants);
+      return restaurants;
+    });  
+  }
+
+
 }
+  // add()
+  // {
+  //   this.cuisins.push({
+  //     name:'Iraqi'
+  //   })
+  // }
+  // update()
+  // {
+  //   this.restaurant=this.af.object('/Restaurant');
+  //   this.restaurant.update({/*if this is set to "set" the object will be override there */
+  //     name:'Hisos',
+  //     Rating:5
+  //   })
+  // }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
 
+export const config = {
+    apiKey: "AIzaSyBokbrS3f62xUt8ROwSUTkwVvF80TGR4JU",
+    authDomain: "pepper-23a55.firebaseapp.com",
+    databaseURL: "https://pepper-23a55.firebaseio.com",
+    projectId: "pepper-23a55",
+    storageBucket: "pepper-23a55.appspot.com",
+    messagingSenderId: "1031911729565"
+  };
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
